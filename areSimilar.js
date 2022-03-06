b7340f24a03708b60ba736004449d9745a840787
KOSSY <abonekosiso@gmail.com>
2022-03-06 23:35:33 +0100

    more

---
+++ areSimilar.js
@@ -0,0 +1,42 @@
+/*
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+Given two arrays a and b, check whether they are similar.
+
+Example
+
+For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+solution(a, b) = true.
+
+The arrays are equal, no need to swap any elements.
+
+For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+solution(a, b) = true.
+
+We can obtain b from a by swapping 2 and 1 in b.
+
+For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+solution(a, b) = false.
+
+Any swap of any two elements either in a or in b won't make a and b equal.
+*/
+
+
+
+function solution(a, b) {
+    let different = []
+    
+    for(let i = 0; i < a.length; i++){
+        if(a[i] != b[i])
+            different.push([a[i], b[i]])
+    }
+    if(different.length == 0) return true
+    
+    if(different.length < 3){
+        const sortA = different[0].sort().join('')
+        const sortB = different[1].sort().join('')
+        
+        return sortA == sortB
+    }
+    return false
+}
